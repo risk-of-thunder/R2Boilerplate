f6409547381957c85f380e2c85dffdd773326db4
dfialkov <fialkovd@gmail.com>
2024-10-06 09:50:56 -0700

    clean things up

--- ToggledChargedGauntlet/Main.cs
+++ ToggledChargedGauntlet/Main.cs
@@ -19,27 +19,11 @@ namespace ToggledChargedGauntlet
     {
         public void Awake()
         {
-            // First we must load our survivor's Body prefab. For this tutorial, we are making a skill for Commando
-            // If you would like to load a different survivor, you can find the key for their Body prefab at the following link
-            // https://xiaoxiao921.github.io/GithubActionCacheTest/assetPathsDump.html
             GameObject loaderBodyPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Loader/LoaderBody.prefab").WaitForCompletion();
-
-
-            // Now we add our skill to one of the survivor's skill families
-            // You can change component.primary to component.secondary, component.utility and component.special
             SkillLocator skillLocator = loaderBodyPrefab.GetComponent<SkillLocator>();
             SkillFamily skillFamily = skillLocator.utility.skillFamily;
 
-            // Now we must create a SkillDef
             skillFamily.variants[0].skillDef.activationState = new SerializableEntityStateType(typeof(ToggledChargeFist));
-
-
-
-
-
-
-
-
         }
     }
 }
